ab58948d8e77d462e57401b4a7885b8d2c149411
angstigone <92314101+angstigone@users.noreply.github.com>
2022-11-10 22:01:52 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,38 @@
-/*!
-* Start Bootstrap - Freelancer v7.0.6 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+function showEur() {
+  var x = document.getElementById("MedeaEur");
+  var y = document.getElementById("MedeaApp");
+  var z = document.getElementById("MedeaOvi");
+  if (x.style.display === "none" || x.style.display === "") {
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+}
 
-window.addEventListener('DOMContentLoaded', event => {
+function showApR() {
+  var x = document.getElementById("MedeaApp");
+  var y = document.getElementById("MedeaEur");
+  var z = document.getElementById("MedeaOvi");
+  if (x.style.display === "none" || x.style.display === "") {
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+}
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
-
-    };
-
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 72,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-});
+function showOvd() {
+  var x = document.getElementById("MedeaOvi");
+  var y = document.getElementById("MedeaEur");
+  var z = document.getElementById("MedeaApp");
+  if (x.style.display === "none" || x.style.display === "") {
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+}

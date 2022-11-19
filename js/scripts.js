7359313084503269d5d7213a357a1cbeef7194dc
angstigone <92314101+angstigone@users.noreply.github.com>
2022-11-19 19:09:17 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -36,3 +36,39 @@ function showOvd() {
     x.style.display = "none";
   }
 }
+
+function drawChartLila() {
+
+    var data = google.visualization.arrayToDataTable([
+      ['Chapter', 'Frequency'],
+      ['Chapter 1', 2],
+      ['Chapter 2', 1],
+      ['Chapter 3', 1],
+      ['Chapter 4', 5],
+      ['Chapter 5', 1],
+      ['Chapter 6', 5],
+      ['Chapter 7', 1],
+      ['Chapter 8', 5],
+      ['Chapter 9', 13],
+      ['Chapter 10', 7],
+      ['Chapter 11', 23],
+      ['Chapter 12', 15],
+      ['Chapter 13', 7],
+      ['Chapter 14', 5],
+      ['Chapter 15', 4],
+      ['Chapter 16', 3]
+      ['Chapter 17', 10],
+      ['Chapter 18', 20],
+      ['Chapter 19', 11],
+      ['Chapter 20', 11],
+      ['Chapter 21', 11]
+    ]);
+    
+    var options = {
+      title: 'Lila'
+    };
+    
+    var chart = new google.visualization.BarChart(document.getElementById('myChart'));
+    chart.draw(data, options);
+    
+    }
